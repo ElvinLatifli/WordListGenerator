2bab2a5bd95ee9b77fc1dd59a1d7b60b41747d5f
Elvin Latifli <elvinlatiflinfo@gmail.com>
2024-03-09 01:53:53 +0300

    fixed

--- install.sh
+++ install.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-INSTALL_DIR="$(pwd)/WordListGenerator"
-
 echo "Gerekli bağımlılıkları kontrol ediliyor ve kuruluyor..."
 apt-get update
 apt-get install -y python3 git
 
 echo "Python dosyasının izinleri ayarlanıyor..."
-chmod +x "$INSTALL_DIR/main.py"
+chmod +x "main.py"
 
 echo "Yürütülebilir dosya PATH'e ekleniyor..."
-sudo cp "$INSTALL_DIR/main.py" /usr/local/bin/hackrick
+sudo cp "main.py" /usr/local/bin/hackrick
 
 echo "Kurulum tamamlandı. Wordlist oluşturucu tool'unuz kullanıma hazır."
 
